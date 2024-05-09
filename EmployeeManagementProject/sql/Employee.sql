29be15e1b1a0feb9890ce9b91fa5140d046c43f8
sunilkumar9696 <162271117+sunilkumar9696@users.noreply.github.com>
2024-05-09 12:56:33 +0530

    Add files via upload

---
+++ EmployeeManagementProject/sql/Employee.sql
@@ -0,0 +1,8 @@
+create table EMPLOYEE(
+Id int not null auto_increment,
+EmployeeName varchar(50) not null,
+EmployeeAddress varchar(80) not null,
+EmployeePhone varchar(50) not null,
+EmployeeSalary varchar(50) not null,
+PRIMARY KEY (Id)
+);
